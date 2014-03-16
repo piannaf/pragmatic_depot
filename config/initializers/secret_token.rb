2c7e5bc922c8a3cd424d0a118e601dfa585cf6d2
Justin Mancinelli <justin.mancinelli@gmail.com>
2014-03-16 16:05:04 +0900

    Initial Commit
    This is just the generated files of the new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '9f0918e57b8c2251bbc8a6c72512e6b576af62c02cba17c6ee77fb1f258ee6d1f42dfeb8a9f2a16478f8b182dcf4fb812470d93a85f832b652e2d8d73b369734'
